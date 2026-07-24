0cbd4257b0ff119746f67648c9e370ce7cdfedfe
Jose Pino <pepepinoortega@gmail.com>
2026-07-24 18:01:55 +0200

    Deployed b8363fb with MkDocs version: 1.6.1

---
+++ assets/site-title.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  document.title = document.title.includes("SportsLabResearch")
+    ? document.title
+    : `${document.title} Â· SportsLabResearch`;
+});
+
